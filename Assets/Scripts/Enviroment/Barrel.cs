a988edb9741efb87c8df29a22ddeb7e84f9b3c22
DanilaAneichik <danila.aneychik2005@gmail.com>
2023-05-31 12:23:09 +0300

    1. Create barrel script. 2. File managment.

--- Assets/Scripts/Enviroment/Barrel.cs
+++ Assets/Scripts/Enviroment/Barrel.cs
@@ -2,5 +2,22 @@
 
 public class Barrel : MonoBehaviour
 {
-        
+    [SerializeField] private GameObject _explosionEffect;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Debug.Log("Collision detected");
+            Explode(collision);
+        }
+    }
+
+    private void Explode(Collision collision)
+    {
+        Instantiate(_explosionEffect, transform.position, Quaternion.identity);
+
+        Destroy(collision.gameObject);
+        Destroy(gameObject);
+    }
 }
